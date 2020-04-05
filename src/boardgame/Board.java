b5c82d0a2a57a0386a0a09aa6be2410c3363652b
strauss7 <wbsabbyyuji@gmail.com>
2020-04-05 16:13:04 -0300

    Handling captured pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -30,19 +30,6 @@ public class Board {
 		return pieces[row][column];
 	}
 	
-	public Piece removePiece(Position position) {
-		if (!positionExists(position)) {
-			throw new BoardException("Position not on the board");
-		}
-		if (piece(position) == null) {
-			return null;
-		}
-		Piece aux = piece(position);
-		aux.position = null;
-		pieces[position.getRow()][position.getColumn()] = null;
-		return aux;
-	}
-	
 	public Piece piece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
@@ -56,6 +43,19 @@ public class Board {
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
 	
 	private boolean positionExists(int row, int column) {
