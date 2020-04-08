45cfe6e8f82abdd0acfe4060a6f3791b79d9f7d1
strauss7 <wbsabbyyuji@gmail.com>
2020-04-08 13:36:28 -0300

    Pawn

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -17,8 +17,8 @@ public class King extends ChessPiece{
 	}
 	
 	private boolean canMove (Position position) {
-		ChessPiece p = (ChessPiece) getBoard().piece(position);
-		return p != null || p.getColor() != getColor();
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
 	}
 
 	@Override
